01a32e467f2bc1f786fb9031daa361b24c64e060
mattjmeier <matthew.james.meier@gmail.com>
2020-12-01 20:55:33 -0500

    Updated params in YAML

--- scripts/render_report.R
+++ scripts/render_report.R
@@ -1,9 +1,20 @@
+### INSTRUCTIONS
+
+# Path
+# Metadata and Contrasts
+# Advanced options
+
+
 # Set custom parameters
 params <- list()
 params$projectdir <- "~/shared/projects/Template_RNASeq/"
 params$species <- "rat"
 params$design <- "group" # Must be a single column of the metadata!
 params$intgroup <- c("group","dose","sex") # Can be multiple columns of interest (covariates, etc)
+params$platform <- "RNA-Seq" # Choose from RNA-Seq or TempO-Seq
+# Add parameter here to specify whether pathway analysis should be run
+# Add parameter for large sample numbers (or automatically detect >48? 96?)
+# Option to limit
 
 # Input file - Rmd
 inputFile <- file.path(params$projectdir, "Rmd", "DESeq2_report.rnaseq.Rmd")
@@ -15,3 +26,11 @@ rmarkdown::render(input = inputFile,
                   output_file = outFile,
                   params = params,
                   envir = new.env())
+
+# For large experiments, loop through contrasts in groups
+# - new param - facet_var for "faceting variable"
+
+
+# Make conda env for ODAF
+#conda create -n myenv python
+#conda install -n myenv scipy=0.15.0
