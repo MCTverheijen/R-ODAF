a7476a26d7d3368dc21ba20d1fe4845b485c85d2
mattjmeier <matthew.james.meier@gmail.com>
2020-12-03 15:06:48 -0500

    Updated params list to allow flexibility and creation of multiple reports

--- scripts/render_report.R
+++ scripts/render_report.R
@@ -1,36 +1,29 @@
-### INSTRUCTIONS
-
-# Path
-# Metadata and Contrasts
-# Advanced options
-
-
-# Set custom parameters
+# Custom parameters for the report
 params <- list()
 params$projectdir <- "~/shared/projects/Template_RNASeq/"
-params$species <- "rat"
-params$design <- "group" # Must be a single column of the metadata!
+params$species <- "rat"     # one of human, mouse, rat, hamster
+params$design <- "group"    # single experimental group of interest
 params$intgroup <- c("group","dose","sex") # Can be multiple columns of interest (covariates, etc)
+params$flag <- TRUE         # runs all analysis by default when set to TRUE
 params$platform <- "RNA-Seq" # Choose from RNA-Seq or TempO-Seq
-# Add parameter here to specify whether pathway analysis should be run
-# Add parameter for large sample numbers (or automatically detect >48? 96?)
-# Option to limit
+params$group_facet <- "sex"  # If you have many different experimental groups, you may subset the report by specifying a column in the metadata to filter groups, and then setting the group of interest in group_filter
+params$group_filter <- "m"           # Which group will this report be done on?
+params$exclude_samples <- c("SRR5890440","SRR5890376")  # Optionally, a vector of sample names to exclude from the analysis
+params$exclude_groups <- c("f2","m2") # Optionally, a vector of groups to exclude from the analysis. By default this is assumed to be in the column specified by params$design.
 
 # Input file - Rmd
 inputFile <- file.path(params$projectdir, "Rmd", "DESeq2_report.rnaseq.Rmd")
+
 # Output file - HTML
 outFile <- file.path(params$projectdir, paste0("reports/RNASeq_analysis_", format(Sys.time(), '%d-%m-%Y.%H.%M'),".html"))
 
-rmarkdown::render(input = inputFile,
-                  encoding = encoding,
-                  output_file = outFile,
-                  params = params,
-                  envir = new.env())
-
-# For large experiments, loop through contrasts in groups
-# - new param - facet_var for "faceting variable"
-
-
-# Make conda env for ODAF
-#conda create -n myenv
-#conda install -n myenv scipy=0.15.0
+if(is.null(params$group_facet)){
+  rmarkdown::render(input = inputFile,
+                    encoding = encoding,
+                    output_file = outFile,
+                    params = params,
+                    envir = new.env())
+} else {
+  message(paste0("Making multiple reports based on ", params$group_facet,"..."))
+  for
+}
