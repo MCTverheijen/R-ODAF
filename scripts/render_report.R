60a79c6039202bee50666e5eba50d33791646dca
mattjmeier <matthew.james.meier@gmail.com>
2020-12-10 11:12:11 -0500

    Added "here" for finding root directory; changed how bmdexpress files are written.

--- scripts/render_report.R
+++ scripts/render_report.R
@@ -2,17 +2,18 @@
 # Custom parameters for the report
 library(tidyverse)
 params <- list()
-params$projectdir <- "~/shared/projects/2020_Long_Rat_TCDD/"
+params$projectdir <- here::here() # If you open this as an R project, it should find your root directory
+params$project_name <- "Flame_Retardants" # Example
 params$species <- "rat"     # one of human, mouse, rat, hamster
-params$design <- "Group"    # single experimental group of interest
-params$intgroup <- c("Group","Treatment","Genotype") # Can be multiple columns of interest (covariates, etc)
+params$design <- "group"    # single experimental group of interest
+params$intgroup <- c("group","dose","sex") # Can be multiple columns of interest (covariates, etc)
 params$flag <- TRUE         # runs all analysis by default when set to TRUE
 params$platform <- "RNA-Seq" # Choose from RNA-Seq or TempO-Seq
-params$group_facet <- NA # NULL # If you have many different experimental groups, you may subset the report by specifying a column in the metadata to filter groups, and then setting the group of interest in group_filter
+params$group_facet <- "sex" # NULL # If you have many different experimental groups, you may subset the report by specifying a column in the metadata to filter groups, and then setting the group of interest in group_filter
 params$exclude_samples <- NA  # Optionally, a vector of sample names to exclude from the analysis
 params$exclude_groups <- NA # Optionally, a vector of groups to exclude from the analysis. By default this is assumed to be in the column specified by params$design.
-params$include_only_column <- "Timepoint" # Restrict analysis to group(s) in the column listed here based on params$include_only_group.
-params$include_only_group <- "5D" # Restrict analysis to this/these group(s) within the column listed in params$include_only_column
+params$include_only_column <- NA # Restrict analysis to group(s) in the column listed here based on params$include_only_group.
+params$include_only_group <- NA # Restrict analysis to this/these group(s) within the column listed in params$include_only_column
 params$use_cached_RData <- FALSE # If possible, load the saved RData for dds object and gene IDs
 params$cpus <- 39 # Set to a lower number (e.g., 2 to 4) if you aren't working in a server environment
 
@@ -31,7 +32,7 @@ if(is.na(params$group_facet)){
 } else {
   SampleKeyFile <- file.path(params$projectdir,"metadata/metadata.txt")
   DESeqDesign <- read.delim(SampleKeyFile,
-                            stringsAsFactors=TRUE,
+                            stringsAsFactors=FALSE,
                             sep="\t",
                             header=TRUE,
                             quote="\"",
