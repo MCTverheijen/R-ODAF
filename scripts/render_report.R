e16de2f189f92dcb8cb0277ccde11d4657783c14
mattjmeier <matthew.james.meier@gmail.com>
2020-12-15 16:13:18 -0500

    Changed params to reflect template

--- scripts/render_report.R
+++ scripts/render_report.R
@@ -3,20 +3,20 @@
 library(tidyverse)
 params <- list()
 params$projectdir <- here::here() # If you open this as an R project, it should find your root directory
-params$project_name <- "Tiparp_TCDD" # Example
+params$project_name <- "Flame_Retardants" # Example
 params$species <- "rat"     # one of human, mouse, rat, hamster
-params$design <- "Group"    # single experimental group of interest
-params$intgroup <- c("Group","Genotype") # Can be multiple columns of interest (covariates, etc)
+params$design <- "group"    # single experimental group of interest
+params$intgroup <- c("group","dose") # Can be multiple columns of interest (covariates, etc)
 params$flag <- TRUE         # runs all analysis by default when set to TRUE
 params$platform <- "RNA-Seq" # Choose from RNA-Seq or TempO-Seq
 params$group_facet <- NA # NULL # If you have many different experimental groups, you may subset the report by specifying a column in the metadata to filter groups, and then setting the group of interest in group_filter
 params$exclude_samples <- NA  # Optionally, a vector of sample names to exclude from the analysis
 params$exclude_groups <- NA # Optionally, a vector of groups to exclude from the analysis. By default this is assumed to be in the column specified by params$design.
-params$include_only_column <- "Timepoint" # Restrict analysis to group(s) in the column listed here based on params$include_only_group.
-params$include_only_group <- "5D" # Restrict analysis to this/these group(s) within the column listed in params$include_only_column
+params$include_only_column <- NA # Restrict analysis to group(s) in the column listed here based on params$include_only_group.
+params$include_only_group <- NA # Restrict analysis to this/these group(s) within the column listed in params$include_only_column
 params$use_cached_RData <- FALSE # If possible, load the saved RData for dds object and gene IDs
 params$cpus <- 39 # Set to a lower number (e.g., 2 to 4) if you aren't working in a server environment
-
+params$run_pathway_analysis <- TRUE
 skip_extra <- #"DMSO 0.1%"
 
 # Input file - Rmd
