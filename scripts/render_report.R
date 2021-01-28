43733d0ab433e6a64dcfd0ab255764f2d2afc3b5
mattjmeier <matthew.james.meier@gmail.com>
2021-01-28 09:59:47 -0500

    Added study-wide QC script improvements

--- scripts/render_report.R
+++ scripts/render_report.R
@@ -6,16 +6,16 @@ params$projectdir <- here::here() # If you open this as an R project, it should 
 params$project_name <- "Bisphenols"
 params$species <- "human"     # one of human, mouse, rat, hamster
 params$design <- "group"    # single experimental group of interest
-params$intgroup <- c("group","dose","pool","batch") # Can be multiple columns of interest (covariates, etc)
+params$intgroup <- c("group","chemical","dose","pool","batch") # Can be multiple columns of interest (covariates, etc)
 params$flag <- TRUE         # runs all analysis by default when set to TRUE
 params$platform <- "TempO-Seq" # Choose from RNA-Seq or TempO-Seq
-params$group_facet <- "chemical" # NULL # If you have many different experimental groups, you may subset the report by specifying a column in the metadata to filter groups, and then setting the group of interest in group_filter
+params$group_facet <- "dose" # NULL # If you have many different experimental groups, you may subset the report by specifying a column in the metadata to filter groups, and then setting the group of interest in group_filter
 params$exclude_samples <- NA  # Optionally, a vector of sample names to exclude from the analysis
-params$exclude_groups <-  c("cells, no treatment",
-                            "No Cells; Brain +ve control",
-                            "No cells; media only",
-                            "No cells; Negative control",
-                            "No cells; Universal human +ve control") # Optionally, a vector of groups to exclude from the analysis. By default this is assumed to be in the column specified by params$design.
+params$exclude_groups <-  # c("Cells, no treatment",
+#                             "No cells; Brain +ve control",
+#                             "No cells; media only",
+#                             "No cells; Negative control",
+#                             "No cells; Universal human +ve control") # Optionally, a vector of groups to exclude from the analysis. By default this is assumed to be in the column specified by params$design.
 params$include_only_column <- NA # Restrict analysis to group(s) in the column listed here based on params$include_only_group.
 params$include_only_group <- NA # Restrict analysis to this/these group(s) within the column listed in params$include_only_column
 params$use_cached_RData <- FALSE # If possible, load the saved RData for dds object and gene IDs
