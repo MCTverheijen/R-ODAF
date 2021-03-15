4d5a5c50e63b8ea7ea734d2b4e47bfb7ea43bed6
mattjmeier <matthew.james.meier@gmail.com>
2021-03-15 11:46:33 -0400

    Added TempoSeqR Script to ODAF Bash Script

--- scripts/pete.star.script_v3.1.R
+++ scripts/pete.star.script_v3.1.R
@@ -1,3 +1,12 @@
+# TempO-SeqR Script Provided by BioSpyder
+# Project-specific customization by Matthew J. Meier
+# Command Line Arguments:
+# 1: FASTA Reference File
+# 2: Directory of FASTQ files to align
+# 3: Number of CPUs to use
+
+# Activate Conda environment for TempO-Seq and Required Software
+system2("conda activate temposeq")
 
 run.alignment.analysis.fn <- function(fasta, queryDirectory, num_clusters){
 
@@ -5,27 +14,29 @@ curr_dir <- getwd()
 logfile <- paste0(curr_dir, "/TempOSeq_log.txt")
 sink(logfile)
 
-#import the Quasr library
+# Import the QuasR library
 library(QuasR) 
 
-#Get full path for fasta file (in case partial path provided to command line)
+# Get full path for FASTA file (in case partial path provided to command line)
 fasta <- system2('readlink', paste0('-f ', fasta), stdout=T)
-
 print(paste0("FASTA: ", fasta))
-#get path of reference directory (trim filename)
+
+# Get path of reference directory (trim filename)
 ref_dir <- dirname(fasta)
 print(paste0("ref_dir: ", ref_dir))
 print(paste0("What happens here?"))
 print(paste0("FASTQ_dir: ", queryDirectory))
-
 print(paste0("Removing old temp files..."))
-#remove tmp files from previous alignments if they are present
+
+###############################################################################
+# Setup folders:
+# Remove temporary files from previous alignments if they are present
 if(dir.exists("_STARtmp")){
    unlink("_STARtmp", recursive=TRUE)
 }
 print(paste0("Done."))
 print(paste0("Creating new temp directories..."))
-#Create temporary directories for temp-alignment files
+# Create temporary directories for temp-alignment files
 dir.create("_STARtmp")
 print(paste0("Changing directory to STARtmp..."))
 setwd("_STARtmp")
@@ -37,84 +48,111 @@ copy_fastqs <- paste0("ln -s ", queryDirectory, "* ./fastqs/")
 system(copy_fastqs)
 queryDirectory <- "fastqs/"
 
-
-#get path of gtf file
+# Get path of GTF annotation file
 annotFile <- gsub("\\.fa", ".gtf", fasta)
 print(paste0("Making cluster"))
-#make the number of clusters you want to use
-cl<- makeCluster(num_clusters)
-
+# Make the number of clusters you want to use
+cl <- makeCluster(num_clusters)
 print(paste0("Getting list of files"))
-#Get list of fastq files
+
+# Get list of FASTQ files
 get_files <- paste0("ls ", queryDirectory, " > samplefile.txt")
 append_files <- paste0("sed -e 's|^|", queryDirectory, "|' -i samplefile.txt")
 system(get_files)
-#add fastq path to file name
-system(append_files)
 
+# Add FASTQ path to file name
+system(append_files)
 print(paste0("Starting alignment function"))
-#Alignment Function
+
+###############################################################################
+# Alignment Function
 aligning.fn <- function(x){
-# test the fastq file: is it zipped or not?
+# Test the FASTQ file: is it zipped or not?
 if(substr(x, (nchar(x)-2),nchar(x))==".gz"){
-# if the fastq is gzipped
-cmd.gz<-paste("STAR --genomeDir ", ref_dir, " --readFilesIn ", x, " --readFilesCommand zcat --runThreadN 7 --outSAMtype BAM SortedByCoordinate --scoreDelOpen -10000 --scoreInsOpen -10000 --outFilterMultimapNmax 1 --outFilterMismatchNmax 2 --outSAMunmapped Within --outFileNamePrefix ", x, sep="")
+# If the FASTQ is gzipped
+cmd.gz <- paste("STAR --genomeDir ",
+                ref_dir,
+                " --readFilesIn ", x,
+                " --readFilesCommand zcat ",
+                " --runThreadN 7 ",
+                " --outSAMtype BAM SortedByCoordinate ",
+                " --scoreDelOpen -10000 ",
+                " --scoreInsOpen -10000 ",
+                " --outFilterMultimapNmax 1 ",
+                " --outFilterMismatchNmax 2 ",
+                " --outSAMunmapped Within ",
+                " --outFileNamePrefix ", x,
+                sep="")
 print(cmd.gz)
 system(cmd.gz)
 
 }else{
-# if the fastq is not zipped
-cmd.notZipped<-paste("STAR --genomeDir ", ref_dir, " --readFilesIn ", x, " --runThreadN 7 --outSAMtype BAM SortedByCoordinate --scoreDelOpen -10000 --scoreInsOpen -10000 --outFilterMultimapNmax 1 --outFilterMismatchNmax 2 --outSAMunmapped Within --outFileNamePrefix ", x, sep="")
+# If the FASTQ is not zipped
+cmd.notZipped <- paste("STAR --genomeDir ",
+                       ref_dir,
+                       " --readFilesIn ", x,
+                       " --runThreadN 7 ",
+                       " --outSAMtype BAM SortedByCoordinate ",
+                       " --scoreDelOpen -10000 ",
+                       " --scoreInsOpen -10000 ",
+                       " --outFilterMultimapNmax 1 ",
+                       " --outFilterMismatchNmax 2 ",
+                       " --outSAMunmapped Within ",
+                       " --outFileNamePrefix ", x,
+                       sep="")
 print(cmd.notZipped)
 system(cmd.notZipped)
 }
-cmd.samtools<-paste("samtools index ",paste(x,"Aligned.sortedByCoord.out.bam",sep=""),sep="")
+cmd.samtools <- paste("samtools index ",paste(x,"Aligned.sortedByCoord.out.bam",sep=""),sep="")
 system(cmd.samtools)
 }
-#End Alignment
+# End Alignment
 
-#import sample file
-sampleF <- read.table("samplefile.txt",header=F,as.is=T)
+###############################################################################
+# Import sample file
+sampleF <- read.table("samplefile.txt", header=F, as.is=T)
 sampleF <- sampleF$V1
-#get sample names
-sampleFile2.bam<-as.data.frame(sampleF)
+# Get sample names
+sampleFile2.bam <- as.data.frame(sampleF)
 
-#Run star Alignment
+# Run star Alignment
 mclapply(sampleF, FUN=aligning.fn, mc.cores=2)
 
-sampleFile2.bam[,1]<-paste0(sampleFile2.bam[,1],"Aligned.sortedByCoord.out.bam")
+sampleFile2.bam[,1] <- paste0(sampleFile2.bam[,1],"Aligned.sortedByCoord.out.bam")
 sampleFile2.bam[,2] <- gsub("fastqs/||\\.fastq.*", "", sampleFile2.bam[,1])
 colnames(sampleFile2.bam) <- c("FileName", "SampleName")
-write.table(sampleFile2.bam,"sample.bam.txt",quote=F,row.names=F,sep="\t")
-sampleFile2.bam.file<-"sample.bam.txt"
+write.table(sampleFile2.bam, "sample.bam.txt", quote=F, row.names=F, sep="\t")
+sampleFile2.bam.file <- "sample.bam.txt"
 
-#create proj file this will make the BAM files, uses BWA internally
-proj2 <- qAlign(sampleFile2.bam.file, fasta, paired="no", cacheDir="proba",clObj=cl)
+# Create proj file:
+# This will make the BAM files, uses BWA internally
+proj2 <- qAlign(sampleFile2.bam.file, fasta, paired="no", cacheDir="proba", clObj=cl)
 
 library(rtracklayer)
 library(GenomicFeatures)
 
 txStart <- import.gff(annotFile, format="gtf")
-names(txStart)<-txStart@seqnames
+names(txStart) <- txStart@seqnames
 
 cnt <- qCount(proj2, txStart)
 
-#make dataframe as count table
+# Make dataframe as count table
 setwd("..")
 cnmat<-as.data.frame(cnt, header=TRUE)
 colnames(cnmat) <- gsub("fastqs/", "", colnames(cnmat))
-cnmat$width<-NULL
+cnmat$width <- NULL
 write.csv(cnmat, "count_table.csv")
 cat("Counts Table Completed\n")
 
-#Get Mapped/Unmapped Counts and Output
-a_frame<-data.frame(alignmentStats(proj2), header=TRUE, check.names=FALSE) 
-unmapped<-a_frame$unmapped 
-mapped<-colSums(cnmat) 
-two<-rbind(mapped) 
-three<-data.frame(two, check.names=FALSE)     
-three$width<-NULL 
-ll<-rbind(unmapped=unmapped, mapped=three)
+# Get Mapped/Unmapped Counts and Output
+a_frame     <- data.frame(alignmentStats(proj2), header=TRUE, check.names=FALSE) 
+unmapped    <- a_frame$unmapped 
+mapped      <- colSums(cnmat) 
+two         <- rbind(mapped) 
+three       <- data.frame(two, check.names=FALSE)     
+three$width <- NULL 
+ll          <- rbind(unmapped=unmapped, mapped=three)
+
 write.csv(as.matrix(ll), "mapped_unmapped.csv")
 cat("Mapped/Unmapped Table Completed\n")
 
@@ -123,19 +161,17 @@ sink()
 
 } #End alignment/analysis function
 
-#Extract Arguments from command line
-args=(commandArgs(TRUE))
-fasta <- args[1]
+###############################################################################
+# Extract Arguments from command line
+args = (commandArgs(TRUE))
+fasta          <- args[1]
 queryDirectory <- args[2]
-num_clusters <- as.numeric(args[3])
+num_clusters   <- as.numeric(args[3])
 
-
-##Run program 
+###############################################################################
+## Run program 
 tryCatch({suppressWarnings(run.alignment.analysis.fn(fasta, queryDirectory, num_clusters))},
 	  error=function(err){
           sink()
 })
-
-
-
 
